e0378063c6798618b6d5143f46ddffd42e7c4de5
chandankrswain <mr.chandanswain@gmail.com>
2025-11-12 02:17:06 +0530

    add : added usescroll to quotes component

--- src/components/quotes/quotes.tsx
+++ src/components/quotes/quotes.tsx
@@ -1,11 +1,14 @@
+import { useScroll } from "@react-three/drei";
+import { motion } from "framer-motion";
+
 export const Quotes = () => {
   return (
-    <div>
-      <p className="text-9xl p-10 ">
+    <motion.div>
+      <motion.p className="text-9xl p-10 ">
         GREAT <span className="text-[#adadad]">DESIGN</span> HAPPENS WHERE LOGIC
         MEETS <span className="text-[#adadad]">EMPATHY</span>.
-      </p>
-    </div>
+      </motion.p>
+    </motion.div>
   );
 };
 
