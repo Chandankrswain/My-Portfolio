42447941b7a0778a32c538277598684617f2bddb
chandankrswain <mr.chandanswain@gmail.com>
2025-11-22 18:29:31 +0530

    fix : fixed the opacity

--- src/components/photo/photo.tsx
+++ src/components/photo/photo.tsx
@@ -3,7 +3,7 @@ import myphoto from "/images/photo.jpg";
 export const MyPhoto = () => {
   return (
     <div>
-      <p className="text-[120px] text-[#adadad] opacity-20   left-18 absolute">
+      <p className="text-[120px] text-[#adadad] opacity-30 left-18 absolute">
         Hello!
       </p>
       <img className=" w-[66%] mx-auto mb-10" src={myphoto} />
