d919a778ac27d58c642878d0eebe1537ad9fa787
chandankrswain <mr.chandanswain@gmail.com>
2025-12-03 00:49:18 +0530

    update : updated  the first quote section for mobile version

--- src/components/quotes/quotes.tsx
+++ src/components/quotes/quotes.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export const Quotes = () => {
   return (
     <motion.div className="mt-2">
-      <motion.p className="text-9xl p-10 leading-36 font-bold">
+      <motion.p className="text-7xl md:text-9xl p-10 leading-20 md:leading-36 font-bold">
         GREAT <span className="text-[#adadad] font-extrabold ">DESIGN</span>{" "}
         HAPPENS WHERE LOGIC MEETS{" "}
         <span className="text-[#adadad font-extrabold ">DESIGN</span>.
