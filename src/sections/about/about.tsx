d71e08865adf5a906db9ff47fb39d738457f167a
chandankrswain <mr.chandanswain@gmail.com>
2025-12-03 00:53:43 +0530

    update :  updated the about section for mobile version

--- src/sections/about/about.tsx
+++ src/sections/about/about.tsx
@@ -2,11 +2,11 @@ import { Button } from "../../components";
 
 export const About = () => {
   return (
-    <div className="h-full flex w-full justify-center border border-[#495057] items-center mb-52 mt-30">
-      <div className="flex">
-        <div className="w-[85%] border border-[#495057] flex flex-col justify-between p-6">
+    <div className="h-full flex flex-col md:flex-row w-full justify-center border border-[#495057] items-center mb-52 mt-30">
+      <div className="flex flex-col md-flex-row ">
+        <div className="md:w-[85%] border border-[#495057] flex flex-col justify-between p-6">
           <div className="w-10 h-8 rounded-full bg-white mb-6"></div>
-          <p className=" text-2xl font-thin mb-6 m-2 text-[#adadad] p-6">
+          <p className=" text-lg md:text-2xl font-thin mb-6 m-2 text-[#adadad] p-6">
             A FRONTEND DEVELOPER WHO LOVES CREATING MODERN AND INTERACTIVE WEB
             EXPERIENCES. WITH A BACKGROUND IN FINANCE I BRING A STRONG FOCUS ON
             DETAIL AND PROBLEM SOLVING TO EVERY PROJECT. MY GOAL IS TO COMBINE
@@ -16,7 +16,9 @@ export const About = () => {
         </div>
 
         <div className="flex flex-col items-end text-right p-8">
-          <p className="text-9xl font-extrabold mb-6">BEHIND THE CODE</p>
+          <p className="text-7xl md:text-9xl font-extrabold mb-6">
+            BEHIND THE CODE
+          </p>
           <Button title="CONTACT ME" />
         </div>
       </div>
