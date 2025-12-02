ec0562c7e03003b455efed0be056f0b671f19c57
chandankrswain <mr.chandanswain@gmail.com>
2025-12-03 02:14:27 +0530

    updated : updated the footer for mobile version , complete the home page for mobile version

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,10 @@
 export const Footer = () => {
   return (
     <div className="mt-10">
-      <p className="text-[250px] text-[#adadad] text-center font-bold">
+      <p className="text-[60px] md:text-[250px] text-[#adadad] text-center font-bold">
         @IM.CKAY
       </p>
-      <div className="flex justify-between mx-20 mb-10">
+      <div className="flex md:flex-row flex-col items-center gap-4 justify-between md:mx-20 mb-10">
         <p>Chandan@portfolio:-$ </p>
         <button onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
           SCROLL TO THE TOP
