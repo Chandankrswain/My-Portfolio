4e30dd6b2a69688955fe6e4482264bb8b1a4ec15
chandankrswain <mr.chandanswain@gmail.com>
2025-12-03 00:46:26 +0530

    update : updated the photo section for mobile version

--- src/components/photo/photo.tsx
+++ src/components/photo/photo.tsx
@@ -3,11 +3,11 @@ import myphoto from "/images/photo.jpg";
 export const MyPhoto = () => {
   return (
     <div className=" mt-20">
-      <p className="text-[120px] text-[#adadad] opacity-30 left-18 absolute z-50">
+      <p className="text-[120px] text-[#adadad] opacity-30 left-18 absolute z-50 hidden md:block">
         Hello!
       </p>
       <img
-        className=" w-[66%] mx-auto mb-10 [clip-path:polygon(70px_0%,100%_0%,100%_calc(100%-70px),calc(100%-70px)_100%,0%_100%,0%_70px)]"
+        className="w-full md:w-[66%] mx-auto mb-10 [clip-path:polygon(70px_0%,100%_0%,100%_calc(100%-70px),calc(100%-70px)_100%,0%_100%,0%_70px)]"
         src={myphoto}
       />
     </div>
