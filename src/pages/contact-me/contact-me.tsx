8fcbfeb067cd3ab70c2d36736b1bc54d3adeabcc
chandankrswain <mr.chandanswain@gmail.com>
2025-12-04 01:07:21 +0530

    update :  added the animation on contact page and complete the project

--- src/pages/contact-me/contact-me.tsx
+++ src/pages/contact-me/contact-me.tsx
@@ -1,15 +1,20 @@
 import { Footer, Form, Navbar } from "../../components";
 import connect from "../../assets/connect.jpg";
+import { AnimatedSection } from "../../sections";
 
 export const ContactPage = () => {
   return (
     <div>
       <Navbar />
       <div className="flex flex-col w-full mt-10 md:mt-20">
-        <p className="text-7xl md:text-[250px] font-extrabold md:leading-55 p-8 md:p-16">
-          CONNECT <span className="text-[#adadad]">WITH</span> ME
-        </p>
-        <Form imageSrc={connect} />
+        <AnimatedSection variant="pop">
+          <p className="text-7xl md:text-[250px] font-extrabold md:leading-55 p-8 md:p-16">
+            CONNECT <span className="text-[#adadad]">WITH</span> ME
+          </p>
+        </AnimatedSection>
+        <AnimatedSection variant="fade-up">
+          <Form imageSrc={connect} />
+        </AnimatedSection>
       </div>
 
       <Footer />
